bfea94f19b1e4fbde68c4b82ba738499441508d3
dariocastroQHR <dario.castro@qhrtech.com>
2024-05-05 16:23:30 -0700

    Commit two

---
+++ Data/CoffeShopDataprovider.cs
@@ -0,0 +1,16 @@
+﻿using WiredBraindCoffe.ShopInfoTool.Model;
+
+namespace WiredBraindCoffe.ShopInfoTool.Data;
+
+public class CoffeShopDataprovider
+{
+    public IEnumerable<CoffeShop> LoadShops()
+    {
+        return new CoffeShop[]
+        {
+            new CoffeShop("Tegu",45),
+            new CoffeShop("SPS",39),
+            new CoffeShop("Sigua",25)
+        };
+    }
+}
